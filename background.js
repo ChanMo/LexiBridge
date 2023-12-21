0a0084a125d7d8905b03a2f921a442a4d7285338
dyou <chen.orange@aliyun.com>
2023-12-21 09:38:43 +0800

    remove contextMenus

--- background.js
+++ background.js
@@ -13,14 +13,14 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   } else if(request.action == 'get-words') {
     chrome.storage.local.get(['words']).then(words => {
       words = words.words ? words.words : []
-      sendResponse(words);      
+      sendResponse(words);
     });
   } else if (request.action == "delete-word") {
     const word = request.data.word;
     chrome.storage.local.get(['words']).then(res => {
       const words = res.words ? res.words : [];
       chrome.storage.local.set({'words': words.filter(i => i[0] !== word)}).then(() => {
-	sendResponse('deleted.');	
+	sendResponse('deleted.');
       });
     });
   }
@@ -33,17 +33,17 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
 //     id: "toggle"
 //   })
 // })
-chrome.contextMenus.onClicked.addListener(async(e, tab) => {
-  const domain = new URL(tab.url).hostname
-  let blocked = await chrome.storage.local.get(['blocked'])
-  blocked = blocked.blocked ? blocked.blocked : []
-  if(e.checked) {
-    chrome.storage.local.set({"blocked": blocked.filter(i => i !== domain)})
-  } else {
-    chrome.storage.local.set({"blocked": [...blocked, domain]})
-  }
-  chrome.storage.local.get(["blocked"]).then(res => {
-    console.log(res)
-  })
-});
-
+//chrome.contextMenus.onClicked.addListener(async(e, tab) => {
+//  const domain = new URL(tab.url).hostname
+//  let blocked = await chrome.storage.local.get(['blocked'])
+//  blocked = blocked.blocked ? blocked.blocked : []
+//  if(e.checked) {
+//    chrome.storage.local.set({"blocked": blocked.filter(i => i !== domain)})
+//  } else {
+//    chrome.storage.local.set({"blocked": [...blocked, domain]})
+//  }
+//  chrome.storage.local.get(["blocked"]).then(res => {
+//    console.log(res)
+//  })
+//});
+//
